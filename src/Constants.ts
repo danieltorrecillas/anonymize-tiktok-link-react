4c46f6b1970fbf14a1f179dde0eab7c388e2bb73
Daniel Torrecillas <22879920+danieltorrecillas@users.noreply.github.com>
2022-05-08 11:02:58 -0500

    feat: add support for environments

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,5 @@
 export const Constants = {
-  apiUrl: 'https://izi9swwb61.execute-api.us-east-1.amazonaws.com/dev/convert',
+  apiUrl: (process.env.REACT_APP_STAGE === 'production' ? 'https://3rce4mrp90.execute-api.us-east-1.amazonaws.com/prd/convert' : 'https://3rce4mrp90.execute-api.us-east-1.amazonaws.com/dev/convert'),
   apiQueryString: '?url=',
   trackingUrl: 'https://vm.tiktok.com/TTPdMcXEWw',
   anonymizedUrl: 'https://m.tiktok.com/v/7022710108492696837',
