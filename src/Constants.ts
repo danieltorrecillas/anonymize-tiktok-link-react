40c7c4d2158303dabd1bcc72bdd0d5d8a253f633
Daniel Torrecillas <22879920+danieltorrecillas@users.noreply.github.com>
2022-05-25 21:40:01 -0500

    fix: edit url formats

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,8 +1,8 @@
 export const Constants = {
   apiUrl: (process.env.REACT_APP_STAGE === 'production' ? 'https://3rce4mrp90.execute-api.us-east-1.amazonaws.com/prd/convert' : 'https://3rce4mrp90.execute-api.us-east-1.amazonaws.com/dev/convert'),
   apiQueryString: '?url=',
-  trackingUrl: 'https://vm.tiktok.com/TTPdMcXEWw',
-  anonymizedUrl: 'https://m.tiktok.com/v/7022710108492696837',
+  trackingUrl: 'https://www.tiktok.com/t/ZTdtTNRjr/?k=1',
+  anonymizedUrl: 'https://www.tiktok.com/@thetalkingbook/video/7053083465318878511',
   author: 'Daniel Torrecillas',
   repositoryLink: 'https://github.com/danieltorrecillas/anonymize-tiktok-link-react'
 } as const
