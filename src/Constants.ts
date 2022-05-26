089495d75d6ff80a302e09e1203b8eceaabe2fa8
Daniel Torrecillas <22879920+danieltorrecillas@users.noreply.github.com>
2022-05-26 07:54:21 -0500

    feat: pull from environment variables for some constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,5 @@
 export const Constants = {
-  apiUrl: (process.env.REACT_APP_STAGE === 'production' ? 'https://3rce4mrp90.execute-api.us-east-1.amazonaws.com/prd/convert' : 'https://3rce4mrp90.execute-api.us-east-1.amazonaws.com/dev/convert'),
+  apiUrl: (process.env.REACT_APP_STAGE === 'production' ? process.env.REACT_APP_API_URL_PRD! : process.env.REACT_APP_API_URL_DEV!),
   apiQueryString: '?url=',
   trackingUrl: 'https://www.tiktok.com/t/ZTdtTNRjr/?k=1',
   anonymizedUrl: 'https://www.tiktok.com/@thetalkingbook/video/7053083465318878511',
